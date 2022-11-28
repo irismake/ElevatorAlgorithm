b4448373fa4a643e877e27c1d7b36fcc014ea025
Iris <iris3455@gmail.com>
2022-11-28 23:49:57 +0900

    Analyzing the number of cases in the other direction
    myDir != elvDir_1

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,13 @@ void main() {
   int min, max;
   int rnd;
   int myDir;
-  int timeStamp;
+  int moveTime = 1;
+  int waitTime = 2;
+  int finalTime;
+
   List<int> via = [];
 
+//나의 층, 목적지 입력하기
   print("Input your floor");
   int? myFloor = int.parse(stdin.readLineSync()!);
   print("Input your destination");
@@ -24,9 +28,10 @@ void main() {
     print("Your direction is up");
     myDir = 1;
   }
-  elvFloor_1 = Random().nextInt(15) - 5;
-  elvDest_1 = Random().nextInt(15) - 5;
-  //min = elvFloor_1 > elvDest_1 ? elvDest_1 : elvFloor_1;
+
+//엘리베이터 움직임 난수로 출력
+  elvFloor_1 = Random().nextInt(14) - 5;
+  elvDest_1 = Random().nextInt(14) - 5;
 
   print("Elevator floor is $elvFloor_1");
   print("Elevator destination is $elvDest_1");
@@ -44,25 +49,42 @@ void main() {
   }
   print(max);
   print(min);
-  elvVia_1 = Random().nextInt(max - min - 1); // 멈출 층 갯수
-
-  while (via.length < elvVia_1) {
-    // 랜덤으로 번호를 생성해준다.
-
-    if (via.length >= elvVia_1) break;
-    rnd = Random().nextInt(max - 1) + (min + 1);
-    print("The floor to pass is $rnd");
-    // 만약 리스트에 생성된 번호가 없다면
-    if (!via.contains(rnd)) {
-      // 리스트에 추가해준다.
-      via.add(rnd);
-    }
-
-    // 리스트의 길이가 6이면 while문을 종료한다.
-
+  if (max - min - 1 == 0) {
+    elvVia_1 = 0;
+  } else {
+    elvVia_1 = Random().nextInt(max - min - 1);
   }
 
-  print(via);
+  print(elvVia_1);
+  while (via.length < elvVia_1) {
+    if (via.length >= elvVia_1) break;
 
-  print("Elevator via is $elvVia_1");
+    rnd = Random().nextInt(max - 1) + (min + 1);
+
+    if (!via.contains(rnd)) {
+      via.add(rnd);
+    }
+  }
+  print("The floor to pass is $via");
+
+//엘리베이터 방향 , 나의 방향 비교
+  if (myDir != elvDir_1) {
+    //다른 방향일때
+    finalTime = moveTime * (max - min) +
+        waitTime * via.length +
+        (elvDest_1 - myFloor).abs() * moveTime;
+  } else {
+    //같은 방향일때
+    if ((min < myFloor) && (myFloor < max)) {
+      // 나를 경유할때
+      if (via.contains(myFloor)) {
+        ;
+      }
+    } else {
+      // 나를 경유하지 않을때
+      finalTime = moveTime * (max - min) +
+          waitTime * via.length +
+          (elvDest_1 - myFloor).abs() * moveTime;
+    }
+  }
 }
